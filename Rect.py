b2c51a1f9b26ba2e8716530708f77857a2d14f12
Vladimir Steshin <warheim24@gmail.com>
2021-05-28 12:53:56 +0300

    Rect.py

---
+++ Rect.py
@@ -0,0 +1,12 @@
+side_square = int(input('Введите длину стороны квадрата: '))
+perimeter_square = (side_square + side_square) * 2
+area_sqare = side_square * side_square
+print('Периметр:', perimeter_square)
+print('Площадь:', area_sqare)
+
+length_rect = int(input('Введите длину прямоугольника: '))
+width_rect = int(input('Введите ширину прямоугольника: '))
+perimeter_rect = (length_rect + width_rect) * 2
+area_rect = length_rect * width_rect
+print('Периметр:', perimeter_rect)
+print('Площадь:', area_rect)
